1ee9038d052bef035e54956b9787c7319780651d
Louis Mathe <mathe.louis@gmail.com>
2018-01-25 20:30:53 +0100

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       root 'docs#index', as: 'authenticated_root'
       root 'welcome#index'
     end
+    root 'welcome#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
